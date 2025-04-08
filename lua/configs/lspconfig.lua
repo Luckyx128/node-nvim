0ac021c8c52ceed9010c96baa7c4330338b86f1e
Lucas <lucas.de.amorimarq@gmail.com>
2025-04-08 09:29:29 -0300

    novos lscp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -36,4 +36,39 @@ lspconfig.pyright.setup {
     },
   },
 }
+lspconfig.intelephense.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    intelephense = {
+      environment = {
+        includePaths = {}, -- você pode adicionar paths extras aqui se quiser
+      },
+      files = {
+        maxSize = 5000000; -- aumenta o limite do tamanho de arquivos se necessário
+      },
+    },
+  },
+}
+lspconfig.dockerls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+lspconfig.yamlls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.yml",
+      },
+      validate = true,
+      hover = true,
+      completion = true,
+    },
+  },
+}
 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Copiar seleção para o clipboard com <leader>y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copiar para o clipboard" })
+
+-- Copiar linha inteira com <leader>Y
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copiar linha para o clipboard" })
+
